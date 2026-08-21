0dbf97e1ad9515d0a80563d5c6dbf813f5a9aea0
Михаил <misha13022008@gmail.com>
2026-08-21 11:25:52 +0300

    feat: implement info collection of globals

--- src/GlobalIVPass.cpp
+++ src/GlobalIVPass.cpp
@@ -4,27 +4,56 @@
 #include "llvm/Plugins/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Analysis/ValueLatticeUtils.h"
+#include "llvm/IR/InstIterator.h"
 
 using namespace llvm;
 
 namespace {
-    class GlobalIVPass : PassInfoMixin<GlobalIVPass> {
+    struct GlobalAccesses {
+        DenseMap<const Function *, SmallVector<const LoadInst *> > loads;
+        DenseMap<const Function *, SmallVector<const StoreInst *> > stores;
+    };
+
+    class GlobalIVPass : public PassInfoMixin<GlobalIVPass> {
         bool can_analyze_global(GlobalVariable &global) {
-            return not global.isConstant()
-                   and global.hasInitializer()
-                   and canTrackGlobalVariableInterprocedurally(&global);
+            return canTrackGlobalVariableInterprocedurally(&global);
         }
 
-    public:
-        PreservedAnalyses run(Module &module, ModuleAnalysisManager &) {
-            errs() << "GlobalIVPass: " << module.getName() << '\n';
-
-            SmallVector<GlobalVariable *> globals;
+        SmallVector<const GlobalVariable *> evaluate_globals(Module &module) {
+            SmallVector<const GlobalVariable *> globals;
 
             for (auto &global: module.globals()) {
                 if (can_analyze_global(global))
                     globals.push_back(&global);
             }
+
+            return globals;
+        }
+
+
+        DenseMap<const GlobalVariable *, GlobalAccesses> evaluate_globals_info(
+            const SmallVector<const GlobalVariable *> &globals_vec
+        ) {
+            DenseMap<const GlobalVariable *, GlobalAccesses> info;
+
+            for (const auto global: globals_vec) {
+                for (const auto user: global->users()) {
+                    if (const auto *load = dyn_cast<LoadInst>(user)) {
+                        info[global].loads[load->getFunction()].push_back(load);
+                    } else if (const auto *store = dyn_cast<StoreInst>(user)) {
+                        info[global].stores[store->getFunction()].push_back(store);
+                    }
+                }
+            }
+
+            return info;
+        }
+
+    public:
+        PreservedAnalyses run(Module &module, ModuleAnalysisManager &) {
+            auto globals_vec = evaluate_globals(module);
+            auto globals_info = evaluate_globals_info(globals_vec);
+
 
             return PreservedAnalyses::all();
         }
